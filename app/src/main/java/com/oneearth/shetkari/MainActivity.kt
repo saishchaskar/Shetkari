4c398cef2c23ebb684828ad198062dc4bd224a3e
SAISH SANTOSHKUMAR CHASKAR <102912746+saishchaskar@users.noreply.github.com>
2024-02-19 21:39:52 +0530

    New Main Activity

--- app/src/main/java/com/oneearth/shetkari/MainActivity.kt
+++ app/src/main/java/com/oneearth/shetkari/MainActivity.kt
@@ -1,319 +1,66 @@
 package com.oneearth.shetkari
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
-import android.os.Build
+
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.EditorInfo
-import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.databinding.DataBindingUtil
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.oneearth.shetkari.Models.WeatherModel
-import com.oneearth.shetkari.R.layout.activity_main
-import com.oneearth.shetkari.Utilites.ApiUtilities
-import com.oneearth.shetkari.databinding.ActivityMainBinding
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import java.math.RoundingMode
-import java.text.SimpleDateFormat
-import java.time.Instant
-import java.time.ZoneId
-import java.util.*
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.oneearth.shetkari.fragments.CropsFragment
+import com.oneearth.shetkari.fragments.MainFragment
+import com.oneearth.shetkari.fragments.ProfileFragment
+import com.oneearth.shetkari.fragments.WeatherFragment
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-
-    private lateinit var pricesIconImageView: ImageView
-    private lateinit var profileImageView: ImageView
-
-    private lateinit var currentLocation: Location
-    private lateinit var fusedLocationProvider: FusedLocationProviderClient
-    private val LOCATION_REQUEST_CODE = 101
-
-    private val apiKey = "886a7d1566beb94eb05472676ea817b0"
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(activity_main)
+        setContentView(R.layout.activity_main)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
-
-        // Get weather data based on user's location
-        getCurrentLocation()
+        // Load the initial fragment (fragment_main) into the frame layout
+        loadFragment(MainFragment())
 
 
-        pricesIconImageView = findViewById(R.id.pricesicon)
-        pricesIconImageView.setOnClickListener {
-            val intent = Intent(this, PricesActivity::class.java)
-            startActivity(intent)
+        val homeImageView: ImageView = findViewById(R.id.Home)
+        val cropImageView: ImageView = findViewById(R.id.Crop)
+        val priceImageView: ImageView = findViewById(R.id.Price)
+        val weatherImageView: ImageView = findViewById(R.id.Weather)
+        val profileImageView: ImageView = findViewById(R.id.Profile)
+
+
+        homeImageView.setOnClickListener {
+            loadFragment(MainFragment())
+        }
+        cropImageView.setOnClickListener {
+            loadFragment(CropsFragment())
         }
 
-        profileImageView = findViewById(R.id.profile)
+        priceImageView.setOnClickListener {
+            loadFragment(ApmcFragment())
+        }
+
+        weatherImageView.setOnClickListener {
+            loadFragment(WeatherFragment())
+        }
+
         profileImageView.setOnClickListener {
-            val intent = Intent(this, RegistrationActivity::class.java)
-            startActivity(intent)
+            loadFragment(ProfileFragment())
         }
 
-        binding.citySearch.setOnEditorActionListener { textView, i, keyEvent ->
-
-            if (i== EditorInfo.IME_ACTION_SEARCH){
-
-                getCityWeather(binding.citySearch.text.toString())
-
-                val view=this.currentFocus
-
-                if (view!=null){
-
-                    val imm: InputMethodManager =getSystemService(INPUT_METHOD_SERVICE)
-                            as InputMethodManager
-
-                    imm.hideSoftInputFromWindow(view.windowToken,0)
-
-                    binding.citySearch.clearFocus()
-
-
-                }
-
-                return@setOnEditorActionListener true
-
-            }
-            else{
-
-                return@setOnEditorActionListener false
-            }
-
-        }
-
-        binding.currentLocation.setOnClickListener {
-            getCurrentLocation()
-        }
     }
 
+    private fun loadFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
 
-    private fun getCityWeather(city: String) {
+        // Replace the existing fragment with the new one
+        transaction.replace(R.id.frame_layout, fragment)
 
-        binding.progressBar.visibility = View.VISIBLE
-
-        ApiUtilities.getApiInterface()?.getCityWeatherData(city, apiKey)
-            ?.enqueue(object : Callback<WeatherModel> {
-                @RequiresApi(Build.VERSION_CODES.O)
-                override fun onResponse(call: Call<WeatherModel>, response: Response<WeatherModel>) {
-                    if (response.isSuccessful) {
-                        binding.progressBar.visibility = View.GONE
-                        response.body()?.let {
-                            setData(it)
-                        }
-                    } else {
-                        Toast.makeText(this@MainActivity, "No City Found", Toast.LENGTH_SHORT).show()
-                        binding.progressBar.visibility = View.GONE
-                    }
-                }
-
-                override fun onFailure(call: Call<WeatherModel>, t: Throwable) {}
-            })
-    }
-
-    private fun fetchCurrentLocationWeather(latitude: String, longitude: String) {
-        ApiUtilities.getApiInterface()?.getCurrentWeatherData(latitude, longitude, apiKey)
-            ?.enqueue(object : Callback<WeatherModel> {
-                @RequiresApi(Build.VERSION_CODES.O)
-                override fun onResponse(call: Call<WeatherModel>, response: Response<WeatherModel>) {
-                    if (response.isSuccessful) {
-                        binding.progressBar.visibility = View.GONE
-                        response.body()?.let {
-                            setData(it)
-                        }
-                    }
-                }
-
-                override fun onFailure(call: Call<WeatherModel>, t: Throwable) {}
-            })
-    }
-
-    private fun getCurrentLocation() {
-        if (checkPermissions()) {
-            if (isLocationEnabled()) {
-                if (ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    requestPermission()
-                    return
-                }
-                fusedLocationProvider.lastLocation
-                    .addOnSuccessListener { location ->
-                        if (location != null) {
-                            currentLocation = location
-                            binding.progressBar.visibility = View.VISIBLE
-                            fetchCurrentLocationWeather(location.latitude.toString(), location.longitude.toString())
-                        }
-                    }
-            } else {
-                val intent = Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-
-                startActivity(intent)
-            }
-        } else {
-            requestPermission()
-        }
-    }
-
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ),
-            LOCATION_REQUEST_CODE
-        )
-    }
-    private fun isLocationEnabled(): Boolean {
-        val locationManager: LocationManager =getSystemService(Context.LOCATION_SERVICE)
-                as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                ||locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-
-    }
-
-    private fun checkPermissions(): Boolean {
-        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)
-            ==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
-            ==PackageManager.PERMISSION_GRANTED){
-            return true
-        }
-        return false
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode==LOCATION_REQUEST_CODE){
-
-            if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                getCurrentLocation()
-            } else{
-            }
-        }
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun setData(body: WeatherModel){
-
-        binding.apply {
-
-            val currentDate=SimpleDateFormat("dd/MM/yyyy hh:mm").format(Date())
-            dateTime.text=currentDate.toString()
-            maxTemp.text="Max "+k2c(body?.main?.temp_max!!)+"°"
-            minTemp.text="Min "+k2c(body?.main?.temp_min!!)+"°"
-            temp.text=""+k2c(body?.main?.temp!!)+"°"
-            weatherTitle.text=body.weather[0].main
-
-
-        }
-        updateUI(body.weather[0].id)
-
-    }
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    private fun ts2td(ts:Long):String{
-
-        val localTime=ts.let {
-
-            Instant.ofEpochSecond(it)
-                .atZone(ZoneId.systemDefault())
-                .toLocalTime()
-        }
-
-        return localTime.toString()
-
-    }
-
-    private fun k2c(t:Double):Double{
-        var intTemp=t
-        intTemp=intTemp.minus(273)
-        return intTemp.toBigDecimal().setScale(1,RoundingMode.UP).toDouble()
-    }
-
-    private fun updateUI(id: Int) {
-        binding.apply {
-            when (id) {
-                //Thunderstorm
-                in 200..232 -> {
-                    weatherImg.setImageResource(R.drawable.ic_storm_weather)
-                    mainLayout.background = ContextCompat.getDrawable(this@MainActivity, R.drawable.thunderstrom_bg)
-
-                }
-                //Drizzle
-                in 300..321 -> {
-                    weatherImg.setImageResource(R.drawable.ic_few_clouds)
-                    mainLayout.background = ContextCompat.getDrawable(this@MainActivity, R.drawable.drizzle_bg)
-
-                }
-                //Rain
-                in 500..531 -> {
-                    weatherImg.setImageResource(R
-                        .drawable.ic_rainy_weather)
-                    mainLayout.background = ContextCompat.getDrawable(this@MainActivity, R.drawable.rain_bg)
-
-                }
-                // Snow
-                in 600..622 -> {
-                    weatherImg.setImageResource(R.drawable.ic_snow_weather)
-                    mainLayout.background = ContextCompat.getDrawable(this@MainActivity, R.drawable.snow_bg)
-
-                }
-                // Atmosphere
-                in 701..781 -> {
-                    weatherImg.setImageResource(R.drawable.ic_broken_clouds)
-                    mainLayout.background = ContextCompat.getDrawable(this@MainActivity, R.drawable.atmosphere_bg)
-
-                }
-                // Clear
-                800 -> {
-                    weatherImg.setImageResource(R.drawable.ic_clear_day)
-                    mainLayout.background = ContextCompat.getDrawable(this@MainActivity, R.drawable.clear_bg)
-
-                }
-                // Clouds
-                in 801..804 -> {
-                    weatherImg.setImageResource(R.drawable.ic_cloudy_weather)
-                    mainLayout.background = ContextCompat.getDrawable(this@MainActivity, R.drawable.clouds_bg)
-
-                }
-                // Unknown
-                else -> {
-                    weatherImg.setImageResource(R.drawable.ic_unknown)
-                    mainLayout.background = ContextCompat.getDrawable(this@MainActivity, R.drawable.unknown_bg)
-
-                }
-            }
-        }
+        // Commit the transaction
+        transaction.commit()
     }
 }
+
+
 
